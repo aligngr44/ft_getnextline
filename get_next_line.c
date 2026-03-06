c915cec2b340f99426910cb2a5c811fd22b09f6d
Ali Güngör <ali4.gngr4@gmail.com>
2026-03-07 02:15:51 +0300

    finish and readme chech

--- get_next_line.c
+++ get_next_line.c
@@ -1,15 +1,23 @@
 #include "get_next_line.h"
 
+static char	*replace_stash(char *stash, char *new_value)
+{
+	free(stash);
+	return (new_value);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*next;
 	int			read_byte;
 	static char	*stash;
 	char		*line;
-	int			len;
+	int			len;	
 	char		buffer[BUFFER_SIZE + 1];
 
 	len = 0;
+	if (!stash)
+   		 stash = ft_strdup("");
 	next = ft_strchr(stash, '\n');
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
@@ -17,20 +25,32 @@ char	*get_next_line(int fd)
 	{
 		read_byte = read(fd, buffer, BUFFER_SIZE);
 		if (read_byte == -1)
+		{
+			free(stash);
+			stash = NULL;
 			return (NULL);
+		}
 		if (read_byte == 0)
 			break ;
 		buffer[read_byte] = '\0';
-		stash = ft_strjoin(stash, buffer);
+		stash = replace_stash(stash, ft_strjoin(stash, buffer));
+		if (!stash)
+			return (NULL);
 		next = ft_strchr(stash, '\n');
 	}
-	if (!stash)
+	if (!stash || stash[0] == '\0')
+	{
+		free(stash);
+		stash = NULL;
 		return (NULL);
+	}
 	if (next)
 		len = (next - stash) + 1;
 	else
 		len = ft_strlen(stash);
 	line = ft_substr(stash, 0, len);
-	stash = ft_substr(stash, len, ft_strlen(stash) - len);
-    return (line);
+	next = ft_substr(stash, len, ft_strlen(stash) - len);
+	free(stash);
+	stash = next;
+	return (line);
 }
